2fd2f0648652045e585cf4990c8683b29575becf
DannaPadilla <dannapc@gmail.com>
2022-09-01 10:18:23 -0500

    commit init

---
+++ Actividad 5/ConsoleMVC/Models/School.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMVC.Models
+{
+    internal class School : Entity
+    {
+        public string Street { get; set; }
+        public List<Student> Students { get; set }
+
+        public School (string Id, string street): base(Id)
+        {
+            Street = street;
+            Students= new List<Student>();
+        }
+        public void AddStudents (Student student)
+        {
+            this.Students.Add(student);
+        }
+    }
+}
