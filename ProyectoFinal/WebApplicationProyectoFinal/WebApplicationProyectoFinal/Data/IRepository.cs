c59d6a006e8f3d8712a074ccc9a8bb667f721321
DannaPadilla <dannapc@gmail.com>
2022-09-11 10:09:09 -0500

    Data

---
+++ ProyectoFinal/WebApplicationProyectoFinal/WebApplicationProyectoFinal/Data/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using WebApplicationProyectoFinal.Models;
+
+namespace WebApplicationProyectoFinal.Data
+{
+    public interface IRepository
+    {
+        public Task Save<T>(T obj) where T: Entity;
+        public void Delete<T>(T obj) where T: Entity;
+        public void Update <T>(T obj) where T: Entity;
+        public Task<List<T>> GetAll<T>() where T : Entity;
+        public Task Commit();
+    }
+}
